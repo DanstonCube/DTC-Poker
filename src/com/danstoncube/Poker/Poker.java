1af709e6f6a3e4e6a390a7484446d32cab4a5dba
DrZoid <miko@miko-dev.(none)>
2011-11-28 13:43:38 +0100

    Squelette bukkit de base

---
+++ src/com/danstoncube/Poker/Poker.java
@@ -0,0 +1,22 @@
+package com.danstoncube.Poker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Poker extends JavaPlugin
+{
+
+	@Override
+	public void onDisable()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
