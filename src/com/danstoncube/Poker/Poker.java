9d0c6fa1b320efd8c2aeec7004e4ad2509073404
DrZoid <miko@miko-dev.(none)>
2011-11-28 13:48:14 +0100

    singleton

--- src/com/danstoncube/Poker/Poker.java
+++ src/com/danstoncube/Poker/Poker.java
@@ -5,18 +5,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Poker extends JavaPlugin
 {
 
+	public static Poker _instance = null;
+	public static Poker getInstance()
+	{
+		return _instance;
+	}
+	
 	@Override
 	public void onDisable()
 	{
 		// TODO Auto-generated method stub
-		
+		Poker._instance = null;
 	}
 
 	@Override
 	public void onEnable()
 	{
 		// TODO Auto-generated method stub
-		
+		Poker._instance = this;
 	}
 
 }
