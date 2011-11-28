8ab20ef989d768434f0c99945da82b621c572533
DrZoid <miko@miko-dev.(none)>
2011-11-28 14:23:06 +0100

    Ajout de toutes les cartes

--- src/com/danstoncube/Poker/CardDeck.java
+++ src/com/danstoncube/Poker/CardDeck.java
@@ -11,17 +11,77 @@ public class CardDeck
 	CardDeck()
 	{
 		cards.add(Card.AS_SPADES);
+		cards.add(Card.TWO_SPADES);
+		cards.add(Card.THREE_SPADES);
+		cards.add(Card.FOUR_SPADES);
+		cards.add(Card.FIVE_SPADES);
+		cards.add(Card.SIX_SPADES);
+		cards.add(Card.SEVEN_SPADES);
+		cards.add(Card.EIGHT_SPADES);
+		cards.add(Card.NINE_SPADES);
+		cards.add(Card.TEN_SPADES);
+		cards.add(Card.JACK_SPADES);
+		cards.add(Card.QUEEN_SPADES);
+		cards.add(Card.KING_SPADES);
 		
-		//TODO: add all cards in Card.java
+		cards.add(Card.AS_HEARTS);
+		cards.add(Card.TWO_HEARTS);
+		cards.add(Card.THREE_HEARTS);
+		cards.add(Card.FOUR_HEARTS);
+		cards.add(Card.FIVE_HEARTS);
+		cards.add(Card.SIX_HEARTS);
+		cards.add(Card.SEVEN_HEARTS);
+		cards.add(Card.EIGHT_HEARTS);
+		cards.add(Card.NINE_HEARTS);
+		cards.add(Card.TEN_HEARTS);
+		cards.add(Card.JACK_HEARTS);
+		cards.add(Card.QUEEN_HEARTS);
+		cards.add(Card.KING_HEARTS);
+		
+		cards.add(Card.AS_DIAMONDS);
+		cards.add(Card.TWO_DIAMONDS);
+		cards.add(Card.THREE_DIAMONDS);
+		cards.add(Card.FOUR_DIAMONDS);
+		cards.add(Card.FIVE_DIAMONDS);
+		cards.add(Card.SIX_DIAMONDS);
+		cards.add(Card.SEVEN_DIAMONDS);
+		cards.add(Card.EIGHT_DIAMONDS);
+		cards.add(Card.NINE_DIAMONDS);
+		cards.add(Card.TEN_DIAMONDS);
+		cards.add(Card.JACK_DIAMONDS);
+		cards.add(Card.QUEEN_DIAMONDS);
+		cards.add(Card.KING_DIAMONDS);
+		
+		cards.add(Card.AS_SPADES);
+		cards.add(Card.TWO_SPADES);
+		cards.add(Card.THREE_SPADES);
+		cards.add(Card.FOUR_SPADES);
+		cards.add(Card.FIVE_SPADES);
+		cards.add(Card.SIX_SPADES);
+		cards.add(Card.SEVEN_SPADES);
+		cards.add(Card.EIGHT_SPADES);
+		cards.add(Card.NINE_SPADES);
+		cards.add(Card.TEN_SPADES);
+		cards.add(Card.JACK_SPADES);
+		cards.add(Card.QUEEN_SPADES);
+		cards.add(Card.KING_SPADES);
 		
 		this.shuffle();
-		
 	}
 	
 	
 	public void shuffle()
 	{
 		//TODO: randomize cards order
+		
+		
+		//tempArray = 
+		
+		//for()
+		
+		
+		
+		
 	}
 	
 	
