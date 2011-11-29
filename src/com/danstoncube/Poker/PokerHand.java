f1bf94e754208e8e25c2edf56dd419c82e900fc0
DrZoid <miko@miko-dev.(none)>
2011-11-29 13:27:29 +0100

    Ajout propriétés dans PokerHand.java (test)

--- src/com/danstoncube/Poker/PokerHand.java
+++ src/com/danstoncube/Poker/PokerHand.java
@@ -1,11 +1,32 @@
 package com.danstoncube.Poker;
 
+import com.danstoncube.Poker.enums.CardEnum;
+
+
+
 public class PokerHand
 {
 	public String id = "";
 	
+	public PokerPlayer winner = null;
+	public PokerPlayer dealer = null;
+	
+	public boolean finished = false;
+	
+	public double smallBlind = 0.0;
+	public double bigBlind = 0.0;
+	
+	public CardEnum[] flop = new CardEnum[5];
+	
+	
+	
+	
 	PokerHand()
 	{
 		this.id = "A RANDOM ID"; //TODO
+		
+		
+		
+		
 	}
 }
