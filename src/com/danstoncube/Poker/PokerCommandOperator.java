c56580e1e5bf78d0e0d6533c637a6f4ecb7de523
DrZoid <miko@miko-dev.(none)>
2011-11-30 02:32:59 +0100

    Ajout etapes d'une main (Enum HandStepEnum)

---
+++ src/com/danstoncube/Poker/PokerCommandOperator.java
@@ -0,0 +1,32 @@
+package com.danstoncube.Poker;
+
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+public class PokerCommandOperator
+{
+	
+	
+	
+	
+	public static boolean joinCommand(Player player, Command cmd, String commandLabel, String[] args)
+	{
+		//TODO
+		return false;
+	}
+	
+	public static boolean createCommand(Player player, Command cmd, String commandLabel, String[] args)
+	{
+		//TODO
+		return false;		
+	}
+
+	public static boolean usageCommand(Player player)
+	{
+		//TODO
+		return false;
+	}
+	
+	
+	
+}
