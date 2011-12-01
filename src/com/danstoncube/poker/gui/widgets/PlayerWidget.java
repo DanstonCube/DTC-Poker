004f1ca960b76a4438b4a2b3aff6795dae5232d7
DrZoid <miko@miko-dev>
2011-12-01 18:04:34 +0100

    ajout playerwidget

---
+++ src/com/danstoncube/poker/gui/widgets/PlayerWidget.java
@@ -0,0 +1,33 @@
+package com.danstoncube.poker.gui.widgets;
+
+import org.getspout.spoutapi.gui.GenericWidget;
+import org.getspout.spoutapi.gui.WidgetType;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+public class PlayerWidget extends GenericWidget
+{
+	private SpoutPlayer player = null;
+		
+	PlayerWidget(SpoutPlayer spoutPlayer)
+	{
+		this.player = spoutPlayer;
+	}
+	
+	public SpoutPlayer getPlayer()
+	{
+		return this.player;
+	}
+	
+	public void setPlayer(SpoutPlayer spoutPlayer)
+	{
+		this.player = spoutPlayer;		
+	}
+	
+	@Override
+	public WidgetType getType()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
